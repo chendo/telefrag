b39da64926fb3177bf1352a30555d0bbacbe01a1
chendo <chendo@chendo.net>
2009-08-17 20:23:27 +1000

    initial commit

---
+++ Telefrag.h
@@ -0,0 +1,18 @@
+//
+//  Telefrag.h
+//  Telefrag
+//
+//  Created by chendo on 17/08/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface Telefrag : NSObject {
+
+}
+
++ (void) load;
+
+@end
---
+++ Telefrag.m
@@ -0,0 +1,18 @@
+//
+//  Telefrag.m
+//  Telefrag
+//
+//  Created by chendo on 17/08/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import "Telefrag.h"
+
+
+@implementation Telefrag
+
++ (void) load {
+  NSLog(@"telefrag loaded");
+}
+
+@end
